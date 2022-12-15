83a884a51cfde08adadf9b5c3db69a7a1ec59543
derrickjhiggins <derrick.higgins@outlook.com>
2022-12-14 18:02:27 -0600

    completed

---
+++ day4_part2.py
@@ -0,0 +1,27 @@
+# modify pair ranges to include any overlap
+
+fully_contained = 0
+
+with open("day4_input.txt") as f:
+    lines = f.readlines()
+
+    # for each line in line:
+        # strip new line character
+        # set variables for first start/end index and second start/end index
+
+    for line in lines:
+        line = line.strip("\n")
+        first_pair, second_pair = line.split(",")
+        first_start_index, first_end_index = first_pair.split("-")
+        second_start_index, second_end_index = second_pair.split("-")
+
+        # if first pair contains any overlap of second pair:
+            # increment fully_contained sum by 1
+        # elif second pair fully contains any overlap first pair:
+            # increment fully_contained sum by 1
+        if int(first_start_index) <= int(second_start_index) <= int(first_end_index) or int(first_start_index) <= int(second_end_index) <= int(first_end_index):
+            fully_contained += 1
+        elif int(second_start_index) <= int(first_start_index) <= int(second_end_index) or int(second_start_index) <= int(first_end_index) <= int(second_end_index):
+            fully_contained += 1
+
+print(fully_contained)
